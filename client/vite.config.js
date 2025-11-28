f22fedf53787fb1537e0930a161f40f1345d62fd
PhilipOndieki <philipbarongo30@gmail.com>
2025-11-28 22:59:39 +0300

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
+    port: 5173,
+    strictPort: true,
   }
 })
